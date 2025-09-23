8c616b5d989e9641c65048dcbc20fbacdf82da70
gaia-tH7 <rm94318@estudante.fieb.edu.br>
2025-09-23 10:43:28 +0000

    commit

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,18 +1,27 @@
-import {useState, useeffect} from 'react'
+// Hooks do React para controlar estado e renderização
+import {useState, useEffect} from 'react'
+// Ferramenta para consumir rotas (endponts) de APIs
 import axios from 'axios'
 
 const Produtos = () => {
-    const[pizzas, setPizzas] = useState([])
-    // Consumir rota com lista de produtos
-    axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
-    .then(response=>{
-         console.log(response.data)
+    // Estado para armazenar a lista de pizzas
+    const [pizzas, setPizzas] = useState(['Calabresa', 'Marguerita', 'Portuguesa'])
 
+    // Consumir rota com lista de produtos
+    // GET - captura os dados do endpoint
+    // then - é uma Promise ou função assíncrona
+    // catch - tratamento do erros encontrados durante requisição
+    useEffect(()=>{
+      axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+    .then(response=>{
+        console.log(response.data.data)
+        setPizzas(response.data.data)
     })
-    .catch(error=>{console.log(error)}
-)
-    // Interação da lista de pizzas - pizza a pizza (um a um)
-    const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
+    .catch(error=>{console.log(error)})
+    },[])
+    
+    // Iteração da lista de pizzas - pizza a pizza (um a um)
+    const listaPizzas = pizzas.map(pizza=><li>{pizza.nome}</li>)
 
     return (
         <div>
